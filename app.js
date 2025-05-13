185a361f1f098af4fb77620ed7d21384e80b1ab2
PL <pl@Mac-mini-de-PL.local>
2025-05-13 17:07:20 +0200

    Suppression log pr chaque requete

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const session = require('express-session');
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(`📥 Nouvelle requête : ${req.method} ${req.url}`);
   next();
 });
 
